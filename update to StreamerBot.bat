8f02938fbc0abf1051bca2b9076c21e3ec554171
Lix <45044873+LixYt@users.noreply.github.com>
2022-08-27 22:50:32 +0200

    Delete update to StreamerBot.bat

--- update to StreamerBot.bat
+++
@@ -1,7 +0,0 @@
-copy bin\Release\TwitchRewarding.exe C:\Users\quent\OneDrive\LixDSL\Tools\StreamerBot\TwitchRewarding.exe
-copy bin\Release\TwitchRewardingEngine.dll C:\Users\quent\OneDrive\LixDSL\Tools\StreamerBot\TwitchRewardingEngine.dll
-
-copy bin\Release\TwitchRewarding.exe C:\Users\quent\NiYoWaZzZz\StreamerBot-NiYo\TwitchRewarding.exe
-copy bin\Release\TwitchRewardingEngine.dll C:\Users\quent\NiYoWaZzZz\StreamerBot-NiYo\TwitchRewardingEngine.dll
-
-pause
